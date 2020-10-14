075a825fdc33161c9ca9debe4689d8cfc525cc17
kaustubh <kaustubh285@gmail.com>
2020-10-14 10:44:30 +0530

    Firebase config file updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDGzYeLHcU9HqpfrcU-4TYUXC1aY5DhkWw",
@@ -9,3 +11,12 @@ const firebaseConfig = {
   appId: "1:1074887178480:web:ffb79771f0df727fc76a35",
   measurementId: "G-6165JS7GFX",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+
+export default db;
