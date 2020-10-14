aa405f93685b9f0a5203c8fcf7eb068d7b2de7c6
kaustubh <kaustubh285@gmail.com>
2020-10-14 10:40:36 +0530

    Chat Input section complete

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,37 @@
 import React from "react";
 import ChatHeader from "./components/ChatHeader";
 import "./css/chat.css";
+import AddCircleRoundedIcon from "@material-ui/icons/AddCircleRounded";
+
+import CardGiftcardRoundedIcon from "@material-ui/icons/CardGiftcardRounded";
+import GifIcon from "@material-ui/icons/Gif";
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
+import ChatMessages from "./components/ChatMessages";
 
 function Chat() {
   return (
     <div className='chat'>
       <ChatHeader />
+      <div className='chat__messages'>
+        <ChatMessages />
+        <ChatMessages />
+        <ChatMessages />
+      </div>
+      <div className='chat__input'>
+        <AddCircleRoundedIcon fontSize='large' />
+        <form>
+          <input type='text' placeholder={`Message #General`} />
+          <button className='chat__inputButton' type='submit'>
+            Send Message
+          </button>
+        </form>
+
+        <div className='chat__inputIcons'>
+          <CardGiftcardRoundedIcon fontSize='large' />
+          <GifIcon fontSize='large' />
+          <EmojiEmotionsIcon fontSize='large' />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGzYeLHcU9HqpfrcU-4TYUXC1aY5DhkWw",
+  authDomain: "discord-clone-b97e1.firebaseapp.com",
+  databaseURL: "https://discord-clone-b97e1.firebaseio.com",
+  projectId: "discord-clone-b97e1",
+  storageBucket: "discord-clone-b97e1.appspot.com",
+  messagingSenderId: "1074887178480",
+  appId: "1:1074887178480:web:ffb79771f0df727fc76a35",
+  measurementId: "G-6165JS7GFX",
+};
