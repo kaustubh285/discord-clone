b29e8e250f2bbf49bde532f15fb6177893646f36
kaustubh <kaustubh285@gmail.com>
2020-10-14 14:05:59 +0530

    Chats can be sent and retrieved

--- src/Chat.js
+++ src/Chat.js
@@ -1,26 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatHeader from "./components/ChatHeader";
 import "./css/chat.css";
 import AddCircleRoundedIcon from "@material-ui/icons/AddCircleRounded";
-
 import CardGiftcardRoundedIcon from "@material-ui/icons/CardGiftcardRounded";
 import GifIcon from "@material-ui/icons/Gif";
 import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
 import ChatMessages from "./components/ChatMessages";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import { selectChannelId, selectChannelName } from "./features/appSlice";
+import db from "./firebase";
+import firebase from "firebase";
 
 function Chat() {
+  const user = useSelector(selectUser);
+  const ChannelId = useSelector(selectChannelId);
+  const ChannelName = useSelector(selectChannelName);
+
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (ChannelId) {
+      db.collection("channels")
+        .doc(ChannelId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) => {
+          setMessages(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
+  }, [ChannelId]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    db.collection("channels").doc(ChannelId).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user,
+    });
+
+    setInput("");
+  };
+
   return (
     <div className='chat'>
-      <ChatHeader />
+      {ChannelName ? (
+        <ChatHeader ChannelName={ChannelName} />
+      ) : (
+        <div
+          style={{
+            height: "75px",
+            color: "white",
+            textDecoration: "underline",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+          <h1>Welcome to Discord Clone</h1>
+        </div>
+      )}
       <div className='chat__messages'>
-        <ChatMessages />
-        <ChatMessages />
-        <ChatMessages />
+        {messages.map((message) => (
+          <ChatMessages
+            timestamp={message.timestamp}
+            user={message.user}
+            message={message.message}
+          />
+        ))}
       </div>
       <div className='chat__input'>
         <AddCircleRoundedIcon fontSize='large' />
-        <form>
-          <input type='text' placeholder={`Message #General`} />
+        <form onSubmit={sendMessage}>
+          <input
+            value={input}
+            disabled={!ChannelId}
+            onChange={(e) => setInput(e.target.value)}
+            type='text'
+            placeholder={`Message #General`}
+          />
           <button className='chat__inputButton' type='submit'>
             Send Message
           </button>
--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -7,16 +7,14 @@ export const appSlice = createSlice({
     channelName: null,
   },
   reducers: {
-    setChannelId: (state, action) => {
-      state.app += action.payload;
-    },
-    logout: (state, action) => {
-      state.app = null;
+    setChannelInfo: (state, action) => {
+      state.channelId = action.payload.channelId;
+      state.channelName = action.payload.channelName;
     },
   },
 });
 
-export const { setChannelId } = appSlice.actions;
+export const { setChannelInfo } = appSlice.actions;
 
 export const selectChannelId = (state) => state.app.channelId;
 export const selectChannelName = (state) => state.app.channelName;
