428568a7026afbe2270c3894c9e198392a287630
kaustubh <kaustubh285@gmail.com>
2020-10-14 12:56:54 +0530

    Authentication complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,12 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import "../css/login.css";
+import { auth, provider } from "../firebase";
 
 function Login() {
-  const signIn = () => {};
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((err) => alert(err.message));
+  };
   return (
     <div className='login'>
       <div className='login__logo'>
@@ -11,6 +14,7 @@ function Login() {
           src='https://media1.tenor.com/images/427100f25fe76c0b971eaa1c65e8561f/tenor.gif'
           alt='dicord'
         />
+        <h1>Clone</h1>
       </div>
       <Button onClick={signIn}>Sign In</Button>
     </div>
